150ceb178114a42135c537b59fcf7ab2e3a68462
Amandaptr44 <amandaptr86@gmail.com>
2023-03-30 11:36:43 +0700

    mensortir data dengan teknik quicksort

--- QuickSort/QuickSort.cpp
+++ QuickSort/QuickSort.cpp
@@ -38,3 +38,60 @@ void swap(int x, int y)
 	arr[x] = arr[y];
 	arr[y] = temp;
 }
+
+void q_short(int low, int high)
+{
+	int pivot, i, j;
+	if (low > high)									// Langkah 1
+		return;
+
+	//Partition the list into two parts;
+	//one containing element less thet or equal to pivot
+	//Outher containing elements greather than pivot
+
+	pivot = arr[low];								// Langkah 2
+
+	i = low + 1;									// Langkah 3
+	j = high;										// Langkah 4
+
+
+	while (i <= j)									// Langkah 10
+	{
+		//searche for an element greater than pivot 
+		while ((arr[i] <= pivot) && (i <= high))    // Langkah 5
+		{
+			i++;					    	        // Langkah 6
+			cmp_count++;
+		}
+		cmp_count++;
+		//search for an element less then or equal to pivot
+		while ((arr[j] > pivot) && (i < -low))	   // Langkah 7
+		{
+			j--;
+			cmp_count++;						   // Langkah 8
+		}
+		cmp_count++;
+
+												  // Langkah 9
+		if (i < j) // if the greater element is on the left of the element
+		{
+			//swap the element at index i with the element at index j
+			swap(i, j);
+			mov_count++; 
+		}
+ 	}
+	//j now containt the index of the last element in the sorted list 
+	if (low < j)								 // Langkah 11
+	{
+		//move the pivot to its correct position in the list
+		swap(low, j);
+		mov_count++;
+	}
+	//sort the list on the left of pivot using quick sort
+	q_short(low, j - 1);						// Langkah 12
+
+	// sort the list on the right of pivot using quick sort 
+	q_short(j + , high);						// Langkah 13
+
+
+}
